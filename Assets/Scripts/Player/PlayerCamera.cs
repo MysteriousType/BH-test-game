d694388a535b2a6ecc209ff8ee488e88aee28fdc
Alexander Belov <73599587+MysteriousType@users.noreply.github.com>
2022-11-01 17:58:18 +0300

    Player camera collision detection (#4)

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -2,6 +2,7 @@
 {
     using UnityEngine;
 
+    [RequireComponent(typeof(Camera))]
     public class PlayerCamera : MonoBehaviour
     {
         [Header("Transforms")]
@@ -21,12 +22,22 @@
         [SerializeField]
         private float _lookSpeed = 2f;
 
+        [Header("Collision Detection Settings")]
+        [SerializeField]
+        private float _collisionDetectionRadius = 0.15f;
+
+        [SerializeField]
+        [Tooltip("How fast the camera should snap into the default position if there are no obstacles")]
+        private float _cameraMoveSpeed = 15f;
+
         private Vector2 _playerRotation;
+        private Vector3 _defaultCameraPosition;
+        private float _defaultCameraPositionDistance;
 
         private void Start()
         {
+            SetupDefaultValues();
             SetupCursor();
-            _playerRotation.y = _playerTransform.eulerAngles.y;
         }
 
         private void Update()
@@ -38,6 +49,27 @@
 
             _playerCameraHolderTransform.localRotation = Quaternion.Euler(_playerRotation.x, 0f, 0f);
             _playerTransform.eulerAngles = new Vector3(0f, _playerRotation.y, 0f);
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 directionToCamera = transform.position - _playerCameraHolderTransform.position;
+
+            if (Physics.SphereCast(_playerCameraHolderTransform.position, _collisionDetectionRadius, directionToCamera, out RaycastHit hit, _defaultCameraPositionDistance))
+            {
+                float distance = hit.distance - _collisionDetectionRadius;
+                transform.localPosition = Vector3.Normalize(_defaultCameraPosition) * distance;
+                return;
+            }
+
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _defaultCameraPosition, _cameraMoveSpeed * Time.deltaTime);
+        }
+
+        private void SetupDefaultValues()
+        {
+            _playerRotation.y = _playerTransform.eulerAngles.y;
+            _defaultCameraPosition = transform.localPosition;
+            _defaultCameraPositionDistance = Vector3.Distance(transform.position, _playerCameraHolderTransform.position);
         }
 
         private void SetupCursor()
