4904556cfa82df7ec6d9fd9ce5b4c4745414742c
Alexander Belov <belov.a.a.a.a@bk.ru>
2022-11-01 18:33:59 +0300

    Update Spawn.meta, PlayerSpawnManager.cs, and PlayerSpawnManager.cs.meta

---
+++ Assets/Scripts/Spawn/PlayerSpawnManager.cs
@@ -0,0 +1,30 @@
+﻿namespace Assets.Scripts.Spawn
+{
+    using UnityEngine;
+
+    public class PlayerSpawnManager : MonoBehaviour
+    {
+        public static PlayerSpawnManager Instance;
+
+        [SerializeField]
+        private Vector3[] _spawnPointPositions;
+
+        public Vector3 GetRandomPosition()
+        {
+            int index = Random.Range(0, _spawnPointPositions.Length);
+            return _spawnPointPositions[index];
+        }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance == this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
