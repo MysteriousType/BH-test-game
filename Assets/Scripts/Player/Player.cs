b36959af6584b6170e85a40e9a0c84097d34868e
Alexander Belov <belov.a.a.a.a@bk.ru>
2022-11-02 18:42:05 +0300

    Update Player.cs and Player.cs.meta

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,53 @@
+﻿namespace Assets.Scripts.Player
+{
+    using Assets.Scripts.Player.Data;
+    using Mirror;
+    using UnityEngine;
+
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(CapsuleCollider))]
+    public class Player : NetworkBehaviour
+    {
+        [Header("Transforms")]
+        [SerializeField]
+        private Transform _playerCameraTransform;
+
+        [SerializeField]
+        private Transform _playerCameraHolderTransform;
+
+        [Header("Data")]
+        [SerializeField]
+        private PlayerData _playerData;
+
+        [SerializeField]
+        private PlayerCameraData _playerCameraData;
+
+        private Rigidbody _playerRigidbody;
+        private CapsuleCollider _playerCapsuleCollider;
+
+        public override void OnStartLocalPlayer()
+        {
+            Camera camera = Camera.main;
+
+            if (camera == null)
+            {
+                Debug.LogError($"MainCamera cannot be found!");
+                return;
+            }
+
+            if (!camera.TryGetComponent(out PlayerCamera playerCamera))
+            {
+                Debug.LogError($"{nameof(PlayerCamera)} component isn't set to the MainCamera!");
+                return;
+            }
+
+            playerCamera.SetupCamera(transform, _playerCameraHolderTransform, _playerCameraData);
+        }
+
+        private void Start()
+        {
+            _playerRigidbody = GetComponent<Rigidbody>();
+            _playerCapsuleCollider = GetComponent<CapsuleCollider>();
+        }
+    }
+}
