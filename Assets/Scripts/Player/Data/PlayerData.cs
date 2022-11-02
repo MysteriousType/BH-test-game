2bccef3159851b16d78ebf3ea1cee2f4d55a8c64
Alexander Belov <belov.a.a.a.a@bk.ru>
2022-11-02 22:59:39 +0300

    Update PlayerData.cs

--- Assets/Scripts/Player/Data/PlayerData.cs
+++ Assets/Scripts/Player/Data/PlayerData.cs
@@ -27,6 +27,9 @@
         [SerializeField]
         private float _dashDistance = 10f;
 
+        [SerializeField]
+        private float _dashHitDuration = 3f;
+
         [Header("Ground Detection")]
         [SerializeField]
         private float _groundCheckDistance = 0.2f;
@@ -50,6 +53,8 @@
         public float DashSpeed => _dashSpeed;
 
         public float DashDistance => _dashDistance;
+
+        public float DashHitDuration => _dashHitDuration;
 
         public float AirSpeedMultiplier => _airSpeedMultiplier;
 
