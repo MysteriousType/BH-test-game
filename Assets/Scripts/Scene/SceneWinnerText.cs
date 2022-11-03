671ee7d7df25c8c6f8e396f2533b42bdb539c384
Alexander Belov <belov.a.a.a.a@bk.ru>
2022-11-03 15:02:39 +0300

    Update Scene.meta, SceneWinnerText.cs, and SceneWinnerText.cs.meta

---
+++ Assets/Scripts/Scene/SceneWinnerText.cs
@@ -0,0 +1,23 @@
+﻿namespace Assets.Scripts.Scene
+{
+    using Mirror;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class SceneWinnerText : NetworkBehaviour
+    {
+        [Header("UI")]
+        [SerializeField]
+        private Text _winnerCanvasText;
+
+        [SyncVar(hook = nameof(OnStatusTextChanged))]
+        private string _text;
+
+        public void SetText(string text) => _text = text;
+
+        private void OnStatusTextChanged(string oldText, string newText)
+        {
+            _winnerCanvasText.text = _text;
+        }
+    }
+}
