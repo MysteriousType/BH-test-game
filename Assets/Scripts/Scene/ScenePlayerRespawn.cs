e63aaac51defc1856f3c2e913edf08a95765f40e
Alexander Belov <belov.a.a.a.a@bk.ru>
2022-11-03 16:17:15 +0300

    Update ScenePlayerRespawn.cs and ScenePlayerRespawn.cs.meta

---
+++ Assets/Scripts/Scene/ScenePlayerRespawn.cs
@@ -0,0 +1,26 @@
+﻿namespace Assets.Scripts.Scene
+{
+    using Assets.Scripts.Player;
+    using Mirror;
+
+    public class ScenePlayerRespawn : NetworkBehaviour
+    {
+        private readonly SyncList<Player> Players = new SyncList<Player>();
+
+        [Command(requiresAuthority = false)]
+        public void AddPlayer(Player player) => Players.Add(player);
+
+        [Command(requiresAuthority = false)]
+        public void RemovePlayer(Player player) => Players.Remove(player);
+
+        [Command(requiresAuthority = false)]
+        public void RespawnAll()
+        {
+            for (int i = 0; i < Players.Count; i++)
+            {
+                Players[i].CmdRespawn();
+                Players[i].RpcRespawn();
+            }
+        }
+    }
+}
